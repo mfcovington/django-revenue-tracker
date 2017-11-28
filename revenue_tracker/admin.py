ef319e37e62992227f04665f78e85cfae6cc0667
Mike Covington <mfcovington@gmail.com>
2017-11-27 23:14:04 -0800

    Add models and admin for Customers, Vendors, and related

--- revenue_tracker/admin.py
+++ revenue_tracker/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact, Country, Customer, Institution, Vendor
+
+
+class CustomerInline(admin.TabularInline):
+    model = Customer
+
+
+class VendorInline(admin.TabularInline):
+    model = Vendor
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'email',
+        'phone',
+        'fax',
+    ]
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [VendorInline]
+    search_fields = ['name']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        'customer',
+        'contact_name',
+        'institution_type',
+        'vendor'
+    ]
+    list_filter = [
+        'institution__institution_type',
+        'institution__country'
+    ]
+    search_fields = [
+        'institution__name',
+        'name',
+        'contact__name',
+        'vendor__name'
+    ]
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    inlines = [CustomerInline]
+    list_display = [
+        'name',
+        'institution_type',
+        'country'
+    ]
+    list_filter = [
+        'institution_type',
+        'country'
+    ]
+    search_fields = ['name']
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    inlines = [CustomerInline]
+    list_display = [
+        'name',
+        'contact_name',
+        'country'
+    ]
+    search_fields = [
+        'name',
+        'contact__name',
+        'country__name'
+    ]
--- revenue_tracker/models.py
+++ revenue_tracker/models.py
@@ -1,3 +1,119 @@
 from django.db import models
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+INSTITUTION_TYPE_CHOICES = [
+    ('Academic', 'Academic'),
+    ('Government', 'Government'),
+    ('Industry', 'Industry'),
+]
+
+
+class Contact(models.Model):
+
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(
+        max_length=255,
+    )
+    email = models.EmailField(
+        blank=True,
+        null=True,
+    )
+    phone = PhoneNumberField(blank=True)
+    fax = PhoneNumberField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Countries'
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+
+    class Meta:
+        ordering = ['institution', 'name']
+
+    institution = models.ForeignKey('Institution')
+    name = models.CharField(
+        blank=True,
+        max_length=255,
+    )
+    contact = models.ForeignKey('Contact')
+    vendor = models.ForeignKey(
+        'Vendor',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.customer
+
+    @property
+    def contact_name(self):
+        return self.contact.name
+
+    @property
+    def customer(self):
+        if self.name == '':
+            return self.institution.name
+        else:
+            return '{} – {}'.format(self.institution.name, self.name)
+
+    @property
+    def institution_type(self):
+        return self.institution.institution_type
+
+
+class Institution(models.Model):
+
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    country = models.ForeignKey('Country')
+    institution_type = models.CharField(
+        choices=INSTITUTION_TYPE_CHOICES,
+        max_length=15,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor(models.Model):
+
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    contact = models.ForeignKey('Contact')
+    country = models.ForeignKey('Country')
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def contact_name(self):
+        return self.contact.name
