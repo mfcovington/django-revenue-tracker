32606a65bf9abf4a8279a1efb9b40ab7e3aac1a5
Mike Covington <mfcovington@gmail.com>
2018-01-08 08:00:19 -0800

    Add form for filtering transactions

--- revenue_tracker/views.py
+++ revenue_tracker/views.py
@@ -16,14 +16,22 @@ class TransactionList(ListView):
             'from_date', dates_fulfilled['date_fulfilled__min'])
         to_date = self.request.GET.get(
             'to_date', dates_fulfilled['date_fulfilled__max'])
+        if from_date == '':
+            from_date = dates_fulfilled['date_fulfilled__min']
+        if to_date == '':
+            to_date = dates_fulfilled['date_fulfilled__max']
         return [from_date, to_date]
 
     def get_context_data(self, **kwargs):
         context = super(TransactionList, self).get_context_data(**kwargs)
         transaction_date_range = self._transaction_date_range()
+        from_date=transaction_date_range[0]
+        to_date=transaction_date_range[1]
         context['report'] = Transaction.objects.get_royalties_report(
-            from_date=transaction_date_range[0],
-            to_date=transaction_date_range[1])
+            from_date=from_date,
+            to_date=to_date)
+        context['from_date'] = str(from_date)
+        context['to_date'] = str(to_date)
         return context
 
     def get_queryset(self):
