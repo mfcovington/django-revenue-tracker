2adfdc88d5d77c9790f9abad654fad4e7fd0ad94
Mike Covington <mfcovington@gmail.com>
2018-09-08 08:15:14 -0700

    Add buttons to remove transaction type or institution type filters

--- revenue_tracker/static/revenue_tracker/js/app.js
+++ revenue_tracker/static/revenue_tracker/js/app.js
@@ -4,3 +4,13 @@ $( "#reset-filter" ).click( function() {
     $( ".radio-label.active" ).removeClass("active");
     $( "input[type=radio]:checked" ).prop("checked", false);
 } );
+
+$( "#reset-institution-type-filter" ).click( function() {
+    $( "#institution-type-filter .radio-label.active" ).removeClass("active");
+    $( "#institution-type-filter input[type=radio]:checked" ).prop("checked", false);
+} );
+
+$( "#reset-transaction-type-filter" ).click( function() {
+    $( "#transaction-type-filter .radio-label.active" ).removeClass("active");
+    $( "#transaction-type-filter input[type=radio]:checked" ).prop("checked", false);
+} );
