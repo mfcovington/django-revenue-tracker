7f0c68f386f38fd20b9ba78914ffe4d5cfcc13b3
Mike Covington <mfcovington@gmail.com>
2018-09-07 20:21:03 -0700

    Switch from jQuery UI's datepicker to bootstrap-datetimepicker
    https://github.com/Eonasdan/bootstrap-datetimepicker

--- revenue_tracker/static/revenue_tracker/js/app.js
+++ revenue_tracker/static/revenue_tracker/js/app.js
@@ -1,16 +1,3 @@
-dateConfig = {
-  buttonText: "<i class='glyphicon glyphicon-calendar'></i>",
-  changeMonth: true,
-  changeYear: true,
-  dateFormat: "yy-mm-dd",
-  showOn: "button"
-};
-
-$( function() {
-  $( "input[name='from_date']" ).datepicker(dateConfig);
-  $( "input[name='to_date']" ).datepicker(dateConfig);
-} );
-
 $( "#reset-filter" ).click( function() {
     $( "#from_date" ).val("");
     $( "#to_date" ).val("");
