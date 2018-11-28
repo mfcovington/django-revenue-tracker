1c2c1fb45296088f9ab70215f0c743ae6c40c477
Mike Covington <mfcovington@gmail.com>
2018-11-28 08:51:02 -0800

    Clean up importing of views for URLs

--- revenue_tracker/urls.py
+++ revenue_tracker/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url
 
-from .views import (
-    CustomerDetail, CustomerList, OutstandingInvoicesList, TransactionDetail,
-    TransactionList)
+from . import views
 
 
 app_name = 'revenue_tracker'
 
 urlpatterns = [
-    url(r'^customer/$', CustomerList.as_view(), name='customer_list'),
-    url(r'^customer/(?P<pk>\d+)/$', CustomerDetail.as_view(), name='customer_detail'),
-    url(r'^outstanding/$', OutstandingInvoicesList.as_view(), name='outstanding_invoices_list'),
-    url(r'^$', TransactionList.as_view(), name='transaction_list'),
-    url(r'^(?P<pk>\d+)/$', TransactionDetail.as_view(), name='transaction_detail'),
+    url(r'^customer/$', views.CustomerList.as_view(), name='customer_list'),
+    url(r'^customer/(?P<pk>\d+)/$', views.CustomerDetail.as_view(), name='customer_detail'),
+    url(r'^outstanding/$', views.OutstandingInvoicesList.as_view(), name='outstanding_invoices_list'),
+    url(r'^$', views.TransactionList.as_view(), name='transaction_list'),
+    url(r'^(?P<pk>\d+)/$', views.TransactionDetail.as_view(), name='transaction_detail'),
 ]
