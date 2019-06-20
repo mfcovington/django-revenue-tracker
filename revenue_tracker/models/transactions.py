f8c486ca82e1571bee9dcb8d4db3b2f51f727af4
Mike Covington <mfcovington@gmail.com>
2019-06-19 22:11:03 -0700

    Add div by zero back to default

--- revenue_tracker/models/transactions.py
+++ revenue_tracker/models/transactions.py
@@ -328,7 +328,7 @@ class RoyaltiesManager(models.Manager):
                     output_field=models.FloatField())),
             sum_number_of_reactions=Sum('number_of_reactions'),
             average_total_price_per_reaction=Case(When(sum_number_of_reactions=0, then=None),
-                default=ExpressionWrapper(1.0 * Sum('total_price'),
+                default=ExpressionWrapper(1.0 * Sum('total_price') / Sum('number_of_reactions'),
                     output_field=MoneyField(
                         decimal_places=2, default_currency='USD', max_digits=8))
             ),
